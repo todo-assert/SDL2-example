033402c6fbb6d7815bac89045100e298201fe7fd
assert <1218715400@qq.com>
2019-05-21 21:10:02 +0800

    add sdl demo for name

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main (int argc, char **argv)
 {
-	monitor_init(640, 480, "TFT Test");
+	monitor_init(640, 480, "SDL2-example");
 	monitor_fill(0, 0, 639, 479, 0xffffffff);
 	while(1);
 	return 0;
